403b9c810f2e93bb08ad525e35e44ad80ec413cd
lathanhphat1911 <5762382@gmail.com>
2026-01-13 20:12:29 +0700

    basecode FE + BE

---
+++ datn_cozypot_spring_boot/src/main/java/com/example/datn_cozypot_spring_boot/DatnCozypotSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.datn_cozypot_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnCozypotSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatnCozypotSpringBootApplication.class, args);
+	}
+
+}
