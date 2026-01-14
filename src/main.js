897e2e5d42c1cb5159644b4c0675f5b0ab14c22a
lathanhphat1911 <5762382@gmail.com>
2026-01-14 20:10:14 +0700

    Sửa format

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
 import '@fortawesome/fontawesome-free/css/all.min.css'
-import router from "./router";
+import router from "./App";
 
 createApp(App).use(router).mount('#app')
