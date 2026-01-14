9e5bb3644e89e59eaff8d3a9596389baccb77308
lathanhphat1911 <5762382@gmail.com>
2026-01-14 20:24:44 +0700

    rename BE

--- datn_cozypot_spring_boot/src/main/java/com/example/datn_cozypot_spring_boot/controller/TestController.java
+++ datn_cozypot_spring_boot/src/main/java/com/example/datn_cozypot_spring_boot/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.example.datn_cozypot_spring_boot.Controller;
+package com.example.datn_cozypot_spring_boot.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
