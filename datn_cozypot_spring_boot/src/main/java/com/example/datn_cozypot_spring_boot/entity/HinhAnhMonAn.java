1617357fc374aa17e3a66e9ad3cd3b879a61df42
casie214 <khuatanhngoc.work@gmail.com>
2026-01-14 21:10:09 +0700

    Thêm Entity

---
+++ datn_cozypot_spring_boot/src/main/java/com/example/datn_cozypot_spring_boot/entity/HinhAnhMonAn.java
@@ -0,0 +1,30 @@
+package com.example.datn_cozypot_spring_boot.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Nationalized;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "hinh_anh_mon_an")
+public class HinhAnhMonAn {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_hinh_anh_mon_an", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_mon_an_di_kem")
+    private MonAnDiKem idMonAnDiKem;
+
+    @Nationalized
+    @Lob
+    @Column(name = "url_anh")
+    private String urlAnh;
+
+    @Column(name = "trang_thai")
+    private Integer trangThai;
+
+}
