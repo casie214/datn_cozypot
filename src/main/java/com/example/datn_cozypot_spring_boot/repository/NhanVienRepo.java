bd40de8c3f887b8d7b99786bf85f27bb49c1f83f
tuanh255 <letuanh819@gmail.com>
2026-02-03 12:33:06 +0700

    refactor: xoa file Repo tao nham va cap nhat KhachHangService

--- src/main/java/com/example/datn_cozypot_spring_boot/repository/NhanVienRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.datn_cozypot_spring_boot.repository;
-
-import com.example.datn_cozypot_spring_boot.entity.NhanVien;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NhanVienRepo extends JpaRepository<NhanVien, Integer> {
-}
